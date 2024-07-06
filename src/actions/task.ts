12b24aeadf96674ac471669bf1360849748f23d1
Shinichi Kanai <shinichi.kanai@twelve-axis.com>
2024-07-06 11:00:50 +0900

    End of 48

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -26,3 +26,38 @@ export const createTask = async (state: FormState, formData: FormData) => {
 
   redirect("/");
 };
+
+export const updateTask = async (
+  id: string,
+  state: FormState,
+  formData: FormData
+) => {
+  const updateTask: Task = {
+    title: formData.get("title") as string,
+    description: formData.get("description") as string,
+    dueDate: formData.get("dueDate") as string,
+    isCompleted: Boolean(formData.get("isCompleted")),
+  };
+
+  try {
+    await connectDb();
+    await TaskModel.updateOne({ _id: id }, updateTask);
+  } catch (error) {
+    state.error = "タスクの更新に失敗しました";
+    return state;
+  }
+
+  redirect("/");
+};
+
+export const deleteTask = async (id: string, state: FormState) => {
+  try {
+    await connectDb();
+    await TaskModel.deleteOne({ _id: id });
+  } catch (error) {
+    state.error = "タスクの削除に失敗しました";
+    return state;
+  }
+
+  redirect("/");
+};
